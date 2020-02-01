db10a3c029216e5ee1fe22db30adb31d083c9622
Andy Stewart <andy.stewart1170@gmail.com>
2020-01-31 17:32:49 -0800

    remove selected class from header nav items

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,7 +6,13 @@ barba.init({
     transitions: [
         {
             name: 'fade',
-            beforeEnter () {
+            beforeEnter ({ current, next, trigger }) {
+                const headerLinks = document.querySelectorAll('header a');
+
+                headerLinks.forEach(link => {
+                    link.classList.remove('selected');
+                })
+
                 window.scrollTo ({
                     top: 0
                 })
